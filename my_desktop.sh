c27803f943213eda9aadadd84988b1befa32dd47
I C <33201750+Natotela@users.noreply.github.com>
2017-12-16 17:58:01 +0200

    Add files via upload
    set workspaces (install compiz)
    set launcher at bottom
    enable hibernation option (hopefully)

---
+++ my_desktop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install compizconfig-settings-manager
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 2
+gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+
+
+sudo echo "
+[Re-enable hibernate by default in upower]
+Identity=unix-user:*
+Action=org.freedesktop.upower.hibernate
+ResultActive=yes
+
+[Re-enable hibernate by default in logind]
+Identity=unix-user:*
+Action=org.freedesktop.login1.hibernate;org.freedesktop.login1.handle-hibernate-key;org.freedesktop.login1;org.freedesktop.login1.hibernate-multiple-sessions;org.freedesktop.login1.hibernate-ignore-inhibit
+ResultActive=yes" >> 1.pkla
+
+sudo mv 1.pkla /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla
